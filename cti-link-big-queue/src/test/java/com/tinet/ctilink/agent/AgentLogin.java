ec2f0f489faa3a8464200e73d4b2badded68dc61
Tinet-AaronAn <anjb@ti-net.com.cn>
2016-05-18 14:01:12 +0800

    增加servlet测试接口

---
+++ cti-link-big-queue/src/test/java/com/tinet/ctilink/agent/AgentLogin.java
@@ -0,0 +1,59 @@
+package com.tinet.ctilink.agent;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.tinet.ctilink.bigqueue.entity.ActionResponse;
+import com.tinet.ctilink.bigqueue.service.imp.AgentServiceImp;
+import com.tinet.ctilink.conf.model.Queue;
+import com.tinet.ctilink.json.JSONObject;
+import com.tinet.ctilink.util.ContextUtil;
+
+@WebServlet("/test/agent/login")
+public class AgentLogin extends HttpServlet {
+
+	AgentServiceImp agentService;
+	@Override
+	public void init() throws ServletException {
+		agentService = ContextUtil.getBean(AgentServiceImp.class);
+	}
+	
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        this.doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("UTF-8");
+        PrintWriter out = resp.getWriter();
+        
+        
+        Map params = new HashMap();
+        params.put("enterpriseId", req.getParameter("enterpriseId"));
+        params.put("cno", req.getParameter("cno"));
+        params.put("bindTel", req.getParameter("bindTel"));
+        params.put("bindType", req.getParameter("bindType"));
+        params.put("loginType", req.getParameter("loginType"));
+        params.put("loginStatus", req.getParameter("loginStatus"));
+
+
+        ActionResponse res = agentService.login(params);
+        if(res != null){
+        	out.print(res.toString());
+        }
+        out.flush();
+        out.close();
+    }
+}
---
+++ cti-link-big-queue/src/test/java/com/tinet/ctilink/agent/AgentPause.java
@@ -0,0 +1,56 @@
+package com.tinet.ctilink.agent;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.tinet.ctilink.bigqueue.entity.ActionResponse;
+import com.tinet.ctilink.bigqueue.service.imp.AgentServiceImp;
+import com.tinet.ctilink.conf.model.Queue;
+import com.tinet.ctilink.json.JSONObject;
+import com.tinet.ctilink.util.ContextUtil;
+
+@WebServlet("/test/agent/login")
+public class AgentPause extends HttpServlet {
+
+	AgentServiceImp agentService;
+	@Override
+	public void init() throws ServletException {
+		agentService = ContextUtil.getBean(AgentServiceImp.class);
+	}
+	
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        this.doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("UTF-8");
+        PrintWriter out = resp.getWriter();
+        
+        
+        Map params = new HashMap();
+        params.put("enterpriseId", req.getParameter("enterpriseId"));
+        params.put("cno", req.getParameter("cno"));
+        params.put("type", req.getParameter("type"));
+        params.put("description", req.getParameter("description"));
+        
+        ActionResponse res = agentService.pause(params);
+        if(res != null){
+        	out.print(res.toString());
+        }
+        out.flush();
+        out.close();
+    }
+}
